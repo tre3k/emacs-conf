ffca368df6233421c03552672c9dccbc2a631b08
Kirill <pshcyrill@mail.ru>
2020-10-25 01:01:45 +0300

    edit setup.sh

--- setup.sh
+++ setup.sh
@@ -6,9 +6,16 @@ INSTALL_USER=${USER}
 function f_Install(){
     echo "install to "${INSTALL_PATH}", for user "${INSTALL_USER}
     install -v -o ${INSTALL_USER} -m640 .emacs ${INSTALL_PATH}/.emacs
-    cp -rv .emacs.d ${INSTALL_PATH}/
-    cp -rv font-victor-mono ${INSTALL_PATH}/.fonts/
+    echo "copy .emacs.d"
+    cp -r .emacs.d ${INSTALL_PATH}/
+    echo "copy fonts"
+    cp -r font-victor-mono ${INSTALL_PATH}/.fonts/
+    echo "done."
 }
 
+case $1 in
+    install)
+	f_Install
+	;;
 
-
+esac
