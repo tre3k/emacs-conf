ebd11ecdaf8cd92c740eb4b849ab3e1771d9ed3f
Kirill <pshcyrill@mail.ru>
2020-10-26 13:04:41 +0300

    change setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,30 @@
 #!/usr/bin/env bash
 
+PACKAGES_GIT_URL="a b c"
+PACKAGES_LOCAL_URL=""
+
+
+CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+function f_init(){
+    echo "Init..."
+    for far in ${PACKAGES_GIT_URL}
+    do
+	
+    done
+   
+}
+
+function f_update(){
+    echo "Update..."
+}
+
+case $1 in
+    init)
+	f_init
+	;;
+    update)
+	f_update
+	;;
+    
+esac
