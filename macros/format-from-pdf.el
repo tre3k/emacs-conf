3c6b8a514f39a743c375b02017fac27df10aa42b
Kirill Pshenichnyi <pshcyrill@mail.ru>
2021-10-14 15:35:59 +0200

    macros: format-from-pdf

--- macros/format-from-pdf.el
+++ macros/format-from-pdf.el
@@ -1,3 +1,24 @@
+(defun replace-pdf-symbols ()
+  (interactive)
+  (save-restriction
+    (narrow-to-region (point) (mark))
+
+    (goto-char (point-min))
+    (while (search-forward "≈" nil t) (replace-match "\\approx" nil t))
+    (goto-char (point-min))
+    (while (search-forward "±" nil t) (replace-match "\\pm" nil t))
+    (goto-char (point-min))
+    (while (search-forward "−" nil t) (replace-match "-" nil t))
+    (goto-char (point-min))
+    (while (search-forward "θ" nil t) (replace-match "$\\theta$" nil t))
+    (goto-char (point-min))
+    (while (search-forward "δ" nil t) (replace-match "$\\delta$" nil t))
+    (goto-char (point-min))
+    (while (search-forward "Γ" nil t) (replace-match "$\\Gamma$" nil t))
+    )
+  )
+
+
 (defun format-from-pdf ()
   "remove newlines and newlines from point"
   (interactive)
@@ -11,21 +32,8 @@
     (while (search-forward ". " nil t)
       (replace-match ".\n" nil t)
       )
-
-    (goto-char (point-min))
-    (while (search-forward "≈" nil t)
-      (replace-match "\\approx" nil t)
-      )
-    (goto-char (point-min))
-    (while (search-forward "±" nil t)
-      (replace-match "\\pm" nil t)
-      )
-   (goto-char (point-min))
-    (while (search-forward "−" nil t)
-      (replace-match "-" nil t)
-      )
     )
   )
 
-
+(provide 'replace-pdf-symbols)
 (provide 'format-from-pdf)
